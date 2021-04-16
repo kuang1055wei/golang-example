8008f14276f2a71f0d3b357d2019cf9c9d1d0da5
kuangwei <xkuangwei@qq.com>
2021-04-16 14:53:18 +0800

    正则表达式

--- 03_channel/context.go
+++ 03_channel/context.go
@@ -8,13 +8,15 @@ import (
 	"time"
 )
 
+//停止协程和context的使用
 func main() {
+	//协程使用案例
 	go MyChan()
 
-	go StopChan()
-
-	go contextStop()
-	time.Sleep(10*time.Second)
+	//go StopChan()
+	//
+	//go contextStop()
+	time.Sleep(10 * time.Second)
 }
 
 func MyChan() {
@@ -23,18 +25,18 @@ func MyChan() {
 	art1Chan := getArt(id1)
 	art2Chan := getArt(id2)
 
-	art1 := <-art1Chan
-	art2 := <-art2Chan
+	//art1 := <-art1Chan
+	//art2 := <-art2Chan
 	data := make(map[string]interface{})
-	data["art1"] = art1
-	data["art2"] = art2
-	fmt.Printf("%+v\n" , data)
+	data["art1"] = <-art1Chan
+	data["art2"] = <-art2Chan
+	fmt.Printf("%+v\n", data)
 }
 
 func getArt(id int) <-chan string {
 	artChan := make(chan string)
 	go func(id int) {
-		art := "article:"+strconv.Itoa(id)
+		art := "article_" + strconv.Itoa(id)
 		artChan <- art
 	}(id)
 	return artChan
@@ -44,21 +46,22 @@ func getArt(id int) <-chan string {
 func StopChan() {
 	var wg sync.WaitGroup
 	wg.Add(1)
-	stopCh := make(chan bool)//停止监控的
+	stopCh := make(chan bool) //停止监控的
 	go func() {
 		defer wg.Done()
-		watchDog(stopCh,"【监控狗】")
+		watchDog(stopCh, "【监控狗】")
 	}()
-	time.Sleep(5*time.Second)
-	stopCh <- true//发送停止指令
+	time.Sleep(5 * time.Second)
+	stopCh <- true //发送停止指令
 	wg.Wait()
 }
 
+//使用通知指令停止协程
 func watchDog(stopCh chan bool, name string) {
 	for {
 		select {
-		case <- stopCh:
-			fmt.Println(name , "停止指令收到，马上停止")
+		case <-stopCh:
+			fmt.Println(name, "停止指令收到，马上停止")
 			return
 		default:
 			fmt.Println(name, "正在监控")
@@ -67,7 +70,7 @@ func watchDog(stopCh chan bool, name string) {
 	}
 }
 
-func contextStop()  {
+func contextStop() {
 	//WithCancel (parent Context)：生成一个可取消的 Context。
 	//WithDeadline (parent Context, d time.Time)：生成一个可定时取消的 Context，参数 d 为定时取消的具体时间。
 	//WithTimeout (parent Context, timeout time.Duration)：生成一个可超时取消的 Context，参数 timeout 用于设置多久后取消
@@ -87,21 +90,22 @@ func contextStop()  {
 	//取消多个
 	var wg sync.WaitGroup
 	wg.Add(2)
-	ctx,stop:=context.WithCancel(context.Background())
+	ctx, stop := context.WithCancel(context.Background())
 	go func() {
 		defer wg.Done()
-		watchDog2(ctx,"【监控狗context】")
+		watchDog2(ctx, "【监控狗context】")
 	}()
-	time.Sleep(5*time.Second)
-	stop()//停止监控狗
+	time.Sleep(5 * time.Second)
+	stop() //停止监控狗
 	wg.Wait()
 }
 
-func watchDog2(ctx context.Context , name string)  {
+//使用context停止协程
+func watchDog2(ctx context.Context, name string) {
 	for {
 		select {
-		case <- ctx.Done():
-			fmt.Println(name , "停止指令收到，马上停止")
+		case <-ctx.Done():
+			fmt.Println(name, "停止指令收到，马上停止")
 			return
 		default:
 			fmt.Println(name, "正在监控")
